f1434fd236b6ada6d9c20eb2605892f5dae65a2b
DanielGoycochea <danielgoycocheardz@gmail.com>
2019-12-28 14:34:55 -0600

    add Fetch

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from "vue";
 import App from "./App.vue";
+import "@/assets/css/tailwind.css";
+import {dolarFilter, percentFilters} from '@/filters'
 
+import router from "@/router";
+Vue.filter ('dolar', dolarFilter);
+Vue.filter ('percent',percentFilters)
 Vue.config.productionTip = false;
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount("#app");
